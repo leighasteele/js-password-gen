07ac5d00770de30bbac81e847c2e64c54e0fea06
Bambi <leighasteele10@gmail.com>
2022-08-24 16:19:34 -0400

    fixed if statements from confirmation

--- script.js
+++ script.js
@@ -4,52 +4,44 @@ var generateBtn = document.querySelector("#generate");
 //Possible Outcomes
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var symbols = ['"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
-var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var lower = 'abcdefghijklmonpqrstuvwxyz';
 var upper = lower.toUpperCase();
 var possible = '';
 var password = '';
-var characters = 8
 
 function generatePassword() {
-  
+
   var characters = prompt('How many characters in your password? (8-128)');
 
   //Prompts
   if (characters >= 8 && characters <= 128) {
-  var hasLower = confirm('Has lowercase letter?');
-  var hasUpper = confirm('Has uppercase latter?');
-  var hasNumber = confirm('Has number?');
-  var hasSpecial = confirm('Has special character?');
-//string of possible characters from prompt selection
-  if (confirm) {
-    for (var i = 0; i <= characters; i++) {
-      var random = Math.floor(Math.random() * numbers.length, symbols.length, lower.length);
-      password += numbers, symbols, lower;
-
-      var possibleCharacters = [];
-
-      if (numbersprompt) {
-        possibleCharacters = possibleCharacters.concat(numbers)
-      }
-      if (symbolprompt) {
-        possibleCharacters = possibleCharacters.concat(symbols)
-      }
-      if (uppercaseprompt) {
-        possibleCharacters = possibleCharacters.concat(uppercase)
-      }
-      if (lowercaseprompt) {
-        possibleCharacters = possibleCharacters.concat(lowercase)
-      }
+    var hasLower = confirm('Has lowercase letter?');
+    var hasUpper = confirm('Has uppercase latter?');
+    var hasNumber = confirm('Has number?');
+    var hasSpecial = confirm('Has special character?');
+    //string of possible characters from prompt selection
+    if (hasLower) {
+      possible += lower;
+    }
+    if (hasUpper) {
+      possible += lower;
+    }
+    if (hasNumber) {
+      possible += lower;
+    }
+    if (hasSpecial) {
+      possible += lower;
     }
   }
-//builds password by randomly selecting string character
-for (var i = 0; i < characters; i++) {
-  var random = Math.floor(Math.random() * possible.length);
-  password += possible[random];
-}
-//shows the actual password
+
+  //builds password by randomly selecting string character
+  for (var i = 0; i < characters; i++) {
+    var random = Math.floor(Math.random() * possible.length);
+    password += possible[random];
+  }
+  //shows the actual password
   return password;
-}}
+};
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
